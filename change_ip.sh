0dcb64c018c8f92fb833e7096d75b1b051d831db
iizukar <czechia.deitan@gmail.com>
2025-02-26 00:47:45 +0100

    Delete change_ip.sh

--- change_ip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while true; do
-  sleep 180
-  echo -e "AUTHENTICATE \"$TOR_CONTROL_PASS\"\r\nSIGNAL NEWNYM\r\nQUIT\r\n" | nc 127.0.0.1 9051
-  
-  # Refresh browser with new IP
-  pkill firefox
-done
